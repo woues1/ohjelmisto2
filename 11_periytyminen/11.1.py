413a4352f1e3e88500ccd6ba85bc3fbe89c36207
tonih <toni.hirvikallio@gmail.com>
2024-03-24 14:19:40 +0200

    asd

--- 11_periytyminen/11.1.py
+++ 11_periytyminen/11.1.py
@@ -5,6 +5,7 @@ class julkaisu:
     def tulosta_tiedot(self):
         print(f"nimi: {self.nimi}")
 
+
 class Kirja(julkaisu):
     def __init__(self, nimi, kirjoittaja, sivumaara):
         self.kirjoittaja = kirjoittaja
@@ -15,6 +16,7 @@ class Kirja(julkaisu):
         super().tulosta_tiedot()
         print(f"Kirjoittaja: {self.kirjoittaja} | sivumaara: {self.sivumaara}")
 
+
 class Lehti(julkaisu):
     def __init__(self, nimi, paatoimittaja):
         self.paatoimittaja = paatoimittaja
@@ -23,10 +25,13 @@ class Lehti(julkaisu):
     def tulosta_tiedot(self):
         super().tulosta_tiedot()
         print(f"Päätoittaja: {self.paatoimittaja}")
+def main():
+    julkaisut = []
+    julkaisut.append(Lehti("Aku Ankka", "Aki Hyyppa"))
+    julkaisut.append(Kirja("Hytti n:o 6", "Rosa Liksom", 200))
 
-julkaisut = []
-julkaisut.append(Lehti("Aku Ankka", "Aki Hyyppa"))
-julkaisut.append(Kirja("Hytti n:o 6", "Rosa Liksom", 200))
+    for julkaisu in julkaisut:
+        julkaisu.tulosta_tiedot()
 
-for julkaisu in julkaisut:
-    julkaisu.tulosta_tiedot()
+if __name__ == "__main__":
+    main()
