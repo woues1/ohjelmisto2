571453786fcc24453f0b7ddba9c6be6b15034cc4
tonih <toni.hirvikallio@gmail.com>
2024-04-08 20:26:08 +0300

    asd

--- Javascript/Moduuli1/5/5.js
+++ Javascript/Moduuli1/5/5.js
@@ -1,16 +1,15 @@
+'use strict';
 
-document.addEventListener('DOMContentLoaded', function(){
-    let year = parseInt(prompt(''));
+document.addEventListener('DOMContentLoaded', function () {
+    let year = parseInt(prompt('Give a year'));
 
-    if(year % 4 == 0){
+    if (year % 4 == 0) {
         alert(`${year} is a leap year`)
-    }
-    else if(year % 100 == 0 && year % 400 == 0){
+    } else if (year % 100 == 0 && year % 400 == 0) {
         alert(`${year} is a leap year`)
-    }
-    else{
+    } else {
         alert(`${year} is not a leap year`)
-    }
+    };
 
 });
 
