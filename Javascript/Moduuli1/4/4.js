964d2f50bb17e86153babfc4f8b8ddd365485843
woues1 <toni.hirvikallio@gmail.com>
2024-04-15 09:05:29 +0300

    asd

--- Javascript/Moduuli1/4/4.js
+++ Javascript/Moduuli1/4/4.js
@@ -3,7 +3,7 @@
 document.addEventListener('DOMContentLoaded',function (){
     function Class(){
         let arr = ['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw']
-        let choice = arr[Math.floor(Math.random()*3)]
+        let choice = arr[Math.floor(Math.random()*3)+1]
         console.log(choice)
         document.getElementById('Harrypotter_class').innerHTML = `${choice}`
     };
--- Javascript/testi1.js
+++ Javascript/testi1.js
@@ -1,12 +1,10 @@
 'use strict';
 
-const isFalse = true == ![];
 const isTrue = true == [];
+const isFalse = true == ![];
 
 console.log(isTrue + isFalse);
 
 
 
-for(let i = 1; i <=10; ++i){
-    console.log(i)
-};
+
